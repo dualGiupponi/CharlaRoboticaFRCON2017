6fc50250b561881de84911f164fc08671c538c68
Juan Blanc <juanblanc14@gmail.com>
2017-08-07 23:30:17 -0300

    Update Servidor_apk_base.ino

--- Ejemplos/Arduino/Servidor_apk_base.ino
+++ Ejemplos/Arduino/Servidor_apk_base.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Automatismo";
-const char* password = "19014760";
+const char* ssid = "IoT";
+const char* password = "alvlgeddl";
 
 ESP8266WebServer server(80);
 
